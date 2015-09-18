89f262c0e7963c9fd30a130884a6dcd367e1f246
Brandon Luong <bluong@palantir.com>
2015-09-18 00:30:18 -0400

    adding initial project files

---
+++ plottable-locale.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="typings/d3/d3.d.ts" />
+/// <reference path="typings/plottable/plottable.d.ts" />
+/// <reference path="typings/moment/moment.d.ts" />
+declare module Plottable {
+    module Locales {
+    }
+}
---
+++ test/references.ts
@@ -0,0 +1,9 @@
+/// <reference path="../typings/d3/d3.d.ts"/>
+/// <reference path="../typings/plottable/plottable.d.ts"/>
+
+/// <reference path="../typings/moment/moment.d.ts"/>
+
+/// <reference path="../plottable-locale.d.ts"/>
+
+/// <reference path="../typings/chai/chai.d.ts"/>
+/// <reference path="../typings/mocha/mocha.d.ts"/>
