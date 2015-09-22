15d4d02c2c0ec46b45b360c49bd6bb52ee9d9792
Brandon Luong <bluong@palantir.com>
2015-09-22 14:07:40 -0700

    adding initial implementation

--- plottable-locale.js
+++ plottable-locale.js
@@ -2,3 +2,136 @@
 /// <reference path="../typings/plottable/plottable.d.ts"/>
 /// <reference path="../typings/moment/moment.d.ts"/> 
 /// <reference path="references.ts"/>
+var Plottable;
+(function (Plottable) {
+    function locale(language) {
+        if (language == null) {
+            return moment.locale();
+        }
+        moment.locale(language);
+    }
+    Plottable.locale = locale;
+    function loadLocale(id, localeDefinition) {
+        var currentLocale = moment.locale();
+        moment.locale(id, localeDefinition);
+        // Loading a locale sets the locale
+        moment.locale(currentLocale);
+    }
+    Plottable.loadLocale = loadLocale;
+    var Locales;
+    (function (Locales) {
+        function defaultTimeAxisConfigurations() {
+            return [
+                [
+                    { interval: Plottable.TimeInterval.second, step: 1, formatter: function (d) { return moment(d).format("LTS"); } },
+                    { interval: Plottable.TimeInterval.day, step: 1, formatter: function (d) { return moment(d).format("LL"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.second, step: 5, formatter: function (d) { return moment(d).format("LTS"); } },
+                    { interval: Plottable.TimeInterval.day, step: 1, formatter: function (d) { return moment(d).format("LL"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.second, step: 10, formatter: function (d) { return moment(d).format("LTS"); } },
+                    { interval: Plottable.TimeInterval.day, step: 1, formatter: function (d) { return moment(d).format("LL"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.second, step: 15, formatter: function (d) { return moment(d).format("LTS"); } },
+                    { interval: Plottable.TimeInterval.day, step: 1, formatter: function (d) { return moment(d).format("LL"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.second, step: 30, formatter: function (d) { return moment(d).format("LTS"); } },
+                    { interval: Plottable.TimeInterval.day, step: 1, formatter: function (d) { return moment(d).format("LL"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.minute, step: 1, formatter: function (d) { return moment(d).format("LT"); } },
+                    { interval: Plottable.TimeInterval.day, step: 1, formatter: function (d) { return moment(d).format("LL"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.minute, step: 5, formatter: function (d) { return moment(d).format("LT"); } },
+                    { interval: Plottable.TimeInterval.day, step: 1, formatter: function (d) { return moment(d).format("LL"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.minute, step: 10, formatter: function (d) { return moment(d).format("LT"); } },
+                    { interval: Plottable.TimeInterval.day, step: 1, formatter: function (d) { return moment(d).format("LL"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.minute, step: 15, formatter: function (d) { return moment(d).format("LT"); } },
+                    { interval: Plottable.TimeInterval.day, step: 1, formatter: function (d) { return moment(d).format("LL"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.minute, step: 30, formatter: function (d) { return moment(d).format("LT"); } },
+                    { interval: Plottable.TimeInterval.day, step: 1, formatter: function (d) { return moment(d).format("LL"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.hour, step: 1, formatter: function (d) { return moment(d).format("LT"); } },
+                    { interval: Plottable.TimeInterval.day, step: 1, formatter: function (d) { return moment(d).format("LL"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.hour, step: 3, formatter: function (d) { return moment(d).format("LT"); } },
+                    { interval: Plottable.TimeInterval.day, step: 1, formatter: function (d) { return moment(d).format("LL"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.hour, step: 6, formatter: function (d) { return moment(d).format("LT"); } },
+                    { interval: Plottable.TimeInterval.day, step: 1, formatter: function (d) { return moment(d).format("LL"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.hour, step: 12, formatter: function (d) { return moment(d).format("LT"); } },
+                    { interval: Plottable.TimeInterval.day, step: 1, formatter: function (d) { return moment(d).format("LL"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.day, step: 1, formatter: function (d) { return moment(d).format("ddd D"); } },
+                    { interval: Plottable.TimeInterval.month, step: 1, formatter: function (d) { return moment(d).format("MMMM YYYY"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.day, step: 1, formatter: function (d) { return moment(d).format("dd D"); } },
+                    { interval: Plottable.TimeInterval.month, step: 1, formatter: function (d) { return moment(d).format("MMMM YYYY"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.day, step: 1, formatter: function (d) { return moment(d).format("D"); } },
+                    { interval: Plottable.TimeInterval.month, step: 1, formatter: function (d) { return moment(d).format("MMMM YYYY"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.month, step: 1, formatter: function (d) { return moment(d).format("MMMM"); } },
+                    { interval: Plottable.TimeInterval.year, step: 1, formatter: function (d) { return moment(d).format("YYYY"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.month, step: 1, formatter: function (d) { return moment(d).format("MMM"); } },
+                    { interval: Plottable.TimeInterval.year, step: 1, formatter: function (d) { return moment(d).format("YYYY"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.month, step: 3, formatter: function (d) { return moment(d).format("MMM"); } },
+                    { interval: Plottable.TimeInterval.year, step: 1, formatter: function (d) { return moment(d).format("YYYY"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.month, step: 6, formatter: function (d) { return moment(d).format("MMM"); } },
+                    { interval: Plottable.TimeInterval.year, step: 1, formatter: function (d) { return moment(d).format("YYYY"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.year, step: 1, formatter: function (d) { return moment(d).format("YYYY"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.year, step: 5, formatter: function (d) { return moment(d).format("YYYY"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.year, step: 25, formatter: function (d) { return moment(d).format("YYYY"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.year, step: 50, formatter: function (d) { return moment(d).format("YYYY"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.year, step: 100, formatter: function (d) { return moment(d).format("YYYY"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.year, step: 250, formatter: function (d) { return moment(d).format("YYYY"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.year, step: 500, formatter: function (d) { return moment(d).format("YYYY"); } }
+                ],
+                [
+                    { interval: Plottable.TimeInterval.year, step: 1000, formatter: function (d) { return moment(d).format("YYYY"); } }
+                ]
+            ];
+        }
+        Locales.defaultTimeAxisConfigurations = defaultTimeAxisConfigurations;
+    })(Locales = Plottable.Locales || (Plottable.Locales = {}));
+})(Plottable || (Plottable = {}));
--- src/locale.ts
+++ src/locale.ts
@@ -1,6 +1,137 @@
 /// <reference path="references.ts"/>
 
 module Plottable {
+
+  export function locale(): string;
+  export function locale(language: string): void;
+  export function locale(language?: string): any {
+    if (language == null) {
+      return moment.locale();
+    }
+    moment.locale(language);
+  }
+
+  export function loadLocale(id: string, localeDefinition: moment.MomentLanguage) {
+    let currentLocale = moment.locale();
+    moment.locale(id, localeDefinition);
+    // Loading a locale sets the locale
+    moment.locale(currentLocale);
+  }
+
   export module Locales {
+
+    export function defaultTimeAxisConfigurations(): Axes.TimeAxisConfiguration[] {
+      return [
+        [
+          {interval: TimeInterval.second, step: 1, formatter: (d) => moment(d).format("LTS")},
+          {interval: TimeInterval.day, step: 1, formatter: (d) => moment(d).format("LL")}
+        ],
+        [
+          {interval: TimeInterval.second, step: 5, formatter: (d) => moment(d).format("LTS")},
+          {interval: TimeInterval.day, step: 1, formatter: (d) => moment(d).format("LL")}
+        ],
+        [
+          {interval: TimeInterval.second, step: 10, formatter: (d) => moment(d).format("LTS")},
+          {interval: TimeInterval.day, step: 1, formatter: (d) => moment(d).format("LL")}
+        ],
+        [
+          {interval: TimeInterval.second, step: 15, formatter: (d) => moment(d).format("LTS")},
+          {interval: TimeInterval.day, step: 1, formatter: (d) => moment(d).format("LL")}
+        ],
+        [
+          {interval: TimeInterval.second, step: 30, formatter: (d) => moment(d).format("LTS")},
+          {interval: TimeInterval.day, step: 1, formatter: (d) => moment(d).format("LL")}
+        ],
+        [
+          {interval: TimeInterval.minute, step: 1, formatter: (d) => moment(d).format("LT")},
+          {interval: TimeInterval.day, step: 1, formatter: (d) => moment(d).format("LL")}
+        ],
+        [
+          {interval: TimeInterval.minute, step: 5, formatter: (d) => moment(d).format("LT")},
+          {interval: TimeInterval.day, step: 1, formatter: (d) => moment(d).format("LL")}
+        ],
+        [
+          {interval: TimeInterval.minute, step: 10, formatter: (d) => moment(d).format("LT")},
+          {interval: TimeInterval.day, step: 1, formatter: (d) => moment(d).format("LL")}
+        ],
+        [
+          {interval: TimeInterval.minute, step: 15, formatter: (d) => moment(d).format("LT")},
+          {interval: TimeInterval.day, step: 1, formatter: (d) => moment(d).format("LL")}
+        ],
+        [
+          {interval: TimeInterval.minute, step: 30, formatter: (d) => moment(d).format("LT")},
+          {interval: TimeInterval.day, step: 1, formatter: (d) => moment(d).format("LL")}
+        ],
+        [
+          {interval: TimeInterval.hour, step: 1, formatter: (d) => moment(d).format("LT")},
+          {interval: TimeInterval.day, step: 1, formatter: (d) => moment(d).format("LL")}
+        ],
+        [
+          {interval: TimeInterval.hour, step: 3, formatter: (d) => moment(d).format("LT")},
+          {interval: TimeInterval.day, step: 1, formatter: (d) => moment(d).format("LL")}
+        ],
+        [
+          {interval: TimeInterval.hour, step: 6, formatter: (d) => moment(d).format("LT")},
+          {interval: TimeInterval.day, step: 1, formatter: (d) => moment(d).format("LL")}
+        ],
+        [
+          {interval: TimeInterval.hour, step: 12, formatter: (d) => moment(d).format("LT")},
+          {interval: TimeInterval.day, step: 1, formatter: (d) => moment(d).format("LL")}
+        ],
+        [
+          {interval: TimeInterval.day, step: 1, formatter: (d) => moment(d).format("ddd D")},
+          {interval: TimeInterval.month, step: 1, formatter: (d) => moment(d).format("MMMM YYYY")}
+        ],
+        [
+          {interval: TimeInterval.day, step: 1, formatter: (d) => moment(d).format("dd D")},
+          {interval: TimeInterval.month, step: 1, formatter: (d) => moment(d).format("MMMM YYYY")}
+        ],
+        [
+          {interval: TimeInterval.day, step: 1, formatter: (d) => moment(d).format("D")},
+          {interval: TimeInterval.month, step: 1, formatter: (d) => moment(d).format("MMMM YYYY")}
+        ],
+        [
+          {interval: TimeInterval.month, step: 1, formatter: (d) => moment(d).format("MMMM")},
+          {interval: TimeInterval.year, step: 1, formatter: (d) => moment(d).format("YYYY")}
+        ],
+        [
+          {interval: TimeInterval.month, step: 1, formatter: (d) => moment(d).format("MMM")},
+          {interval: TimeInterval.year, step: 1, formatter: (d) => moment(d).format("YYYY")}
+        ],
+        [
+          {interval: TimeInterval.month, step: 3, formatter: (d) => moment(d).format("MMM")},
+          {interval: TimeInterval.year, step: 1, formatter: (d) => moment(d).format("YYYY")}
+        ],
+        [
+          {interval: TimeInterval.month, step: 6, formatter: (d) => moment(d).format("MMM")},
+          {interval: TimeInterval.year, step: 1, formatter: (d) => moment(d).format("YYYY")}
+        ],
+        [
+          {interval: TimeInterval.year, step: 1, formatter: (d) => moment(d).format("YYYY")}
+        ],
+        [
+          {interval: TimeInterval.year, step: 5, formatter: (d) => moment(d).format("YYYY")}
+        ],
+        [
+          {interval: TimeInterval.year, step: 25, formatter: (d) => moment(d).format("YYYY")}
+        ],
+        [
+          {interval: TimeInterval.year, step: 50, formatter: (d) => moment(d).format("YYYY")}
+        ],
+        [
+          {interval: TimeInterval.year, step: 100, formatter: (d) => moment(d).format("YYYY")}
+        ],
+        [
+          {interval: TimeInterval.year, step: 250, formatter: (d) => moment(d).format("YYYY")}
+        ],
+        [
+          {interval: TimeInterval.year, step: 500, formatter: (d) => moment(d).format("YYYY")}
+        ],
+        [
+          {interval: TimeInterval.year, step: 1000, formatter: (d) => moment(d).format("YYYY")}
+        ]
+      ];
+    }
+
   }
 }
