d1dd523dbc88545a66c906e6f4a23e405dcffb30
Brandon Luong <bluong@palantir.com>
2015-09-25 16:18:52 -0700

    adding built file

--- plottable-moment.js
+++ plottable-moment.js
@@ -1,4 +1,4 @@
 /// <reference path="../typings/d3/d3.d.ts"/>
 /// <reference path="../typings/plottable/plottable.d.ts"/>
-/// <reference path="../typings/moment/moment.d.ts"/>
+/// <reference path="../typings/moment/moment.d.ts"/> 
 /// <reference path="references.ts"/>
