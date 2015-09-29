fd7a7704064b4f0ae99574a2610426ea66449e98
Brandon Luong <bluong@palantir.com>
2015-09-29 11:55:04 -0700

    more small changes

--- plottable-moment.js
+++ plottable-moment.js
@@ -1,6 +1,6 @@
 /// <reference path="../typings/d3/d3.d.ts"/>
 /// <reference path="../typings/plottable/plottable.d.ts"/>
-/// <reference path="../typings/moment/moment.d.ts"/> 
+/// <reference path="../typings/moment/moment.d.ts"/>
 /// <reference path="references.ts"/>
 var Plottable;
 (function (Plottable) {
